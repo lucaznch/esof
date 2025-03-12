f4ffb74609f75581d980b2e95f748760c5830f23
lucaznch <lucazanchisilva@gmail.com>
2025-03-12 14:26:39 +0000

    feat(activity-suggestion-service): implement ActivitySuggestion service layer #26

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activitysuggestion/ActivitySuggestionService.java
@@ -0,0 +1,65 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.repository.InstitutionRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.domain.ActivitySuggestion;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.dto.ActivitySuggestionDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.repository.ActivitySuggestionRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.*;
+
+import java.util.List;
+import java.util.Comparator;
+
+
+@Service
+public class ActivitySuggestionService {
+    @Autowired
+    ActivitySuggestionRepository activitySuggestionRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    InstitutionRepository institutionRepository;
+
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public List<ActivitySuggestionDto> getActivitySuggestions() {
+        return activitySuggestionRepository.findAll().stream()
+                .map(s -> new ActivitySuggestionDto(s, true, true))
+                .sorted(Comparator.comparing(ActivitySuggestionDto::getName, String.CASE_INSENSITIVE_ORDER))
+                .toList();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public List<ActivitySuggestionDto> getActivitySuggestionsByInstitution(Integer institutionId) {
+        if (institutionId == null || !institutionRepository.existsById(institutionId)) throw new HEException(INSTITUTION_NOT_FOUND, institutionId);
+        
+        return activitySuggestionRepository.getActivitySuggestionsByInstitution(institutionId).stream()
+                .map(s -> new ActivitySuggestionDto(s, true, true))
+                .sorted(Comparator.comparing(ActivitySuggestionDto::getName, String.CASE_INSENSITIVE_ORDER))
+                .toList();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public ActivitySuggestionDto createActivitySuggestion(Integer userId, Integer institutionId, ActivitySuggestionDto activitySuggestionDto) {
+        if (userId == null) throw new HEException(USER_NOT_FOUND);
+        Volunteer volunteer = (Volunteer) userRepository.findById(userId).orElseThrow(() -> new HEException(USER_NOT_FOUND, userId));
+
+        if (institutionId == null) throw new HEException(INSTITUTION_NOT_FOUND);
+        Institution institution = (Institution) institutionRepository.findById(institutionId).orElseThrow(() -> new HEException(INSTITUTION_NOT_FOUND, institutionId));
+
+        ActivitySuggestion activitySuggestion = new ActivitySuggestion(institution, volunteer, activitySuggestionDto);
+
+        activitySuggestionRepository.save(activitySuggestion);
+
+        return new ActivitySuggestionDto(activitySuggestion, true, true);
+    }
+}
--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activitysuggestion/repository/ActivitySuggestionRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activitysuggestion/repository/ActivitySuggestionRepository.java
@@ -3,13 +3,17 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.reposi
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.domain.ActivitySuggestion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Repository
 @Transactional
 public interface ActivitySuggestionRepository extends JpaRepository<ActivitySuggestion, Integer> {
-
+    @Query("SELECT a FROM ActivitySuggestion a WHERE a.institution.id = :institutionId")
+    List<ActivitySuggestion> getActivitySuggestionsByInstitution(Integer institutionId);
 }
 
